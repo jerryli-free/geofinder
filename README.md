2a11fa9e6c729573b39042208dafefe6d7850472
jerryli-free <70525391+jerryli-free@users.noreply.github.com>
2020-09-01 14:12:28 +0800

    maint:路由修改

--- README.md
+++ README.md
@@ -15,43 +15,40 @@ v1.1版本目前可以查询国内的省份，直辖市，特区，和城市
 
 cd public;
 
-go run build -o gps Main.go 
-
-
-
-## Docker安装
-
-docker pull registry.cn-hangzhou.aliyuncs.com/cb-repository/gpsservice:v1(私有仓库的密码咨询祖振)
-docker run --name gpsservice -d  -p 1230:1230 -v /home/log/gpsServer:/home/log/gpsServer registry.cn-hangzhou.aliyuncs.com/cb-repository/gpsservice:v1
+go build -o geofinder Main.go 
 
 
 
 # 启动
-
-执行二进制文件，nohup ./gps -osmpath ../service/osm/osmData/ &参数如下
+cd public
+go build -o geofinder Main.go 
+执行二进制文件，./geofinder 参数如下
 
   -acclog string
     	访问日志path（可不填，默认在/home/log/gpsServer/http_access.log）
   -osmpath string
-    	osm文件目录 (default "osmData/"，必须项目，osm文件是程序加载的数据文件)
+    	osm文件目录 (default "../service/osm/osmData/"，必须项目，osm文件是程序加载的数据文件)
   -port int
     	指定端口号 (default 1230)
 
-## 访问
+## 使用
 
-curl '127.0.0.1:1230/?ac=GetGpsInfo&lat=39.884159&lon=117.010229'
+curl '127.0.0.1:1230/getcity?lat=39.935297&lon=117.119176'
 
-## 返回值
+## 返回值说明
 
-{"errorCode":0,"errorMsg":"ok","result":{"Province_en":"Shandong","Province_zh":"山东省","City_en":"Dongying City","City_zh":"东营市","District_en":"Lijin County","District_zh":"利津县","Similar":1}}
-
-errorCode:0代表成功，-1代表失败
-errorMsg：错误内容，成功为ok
-
-Province_en：省份的英文名称
-Province_zh：省份的中文名称
-City_en：城市英文
-City_zh：城市中文
-District_en：区县英文
-District_zh：区县中文
-Similar：是否为近似查找
+{
+    "errorCode": 0,//0代表成功，-1代表失败
+    "errorMsg": "ok",//错误内容，成功为ok
+    "result": {
+        "Province_en": "Hebei",//省份的英文名称
+        "Province_zh": "河北省",//省份的中文名称
+        "City_en": "Langfang City",//城市英文
+        "City_zh": "廊坊市",//城市中文
+        "District_en": "Sanhe City",//区县英文
+        "District_zh": "三河市",//区县中文
+        "Similar": 0//1为准确查找，1为近似查找，就是查找距离输入点最近的城市
+		"lat": "39.935297",
+        "lon": "117.119176"
+    }
+}
--- app/api.go
+++ app/api.go
@@ -30,7 +30,7 @@ func (g *GpsServer) Start() {
 	app.Use(lib.NewUseLog(g.AccessLogPath), gin.Recovery())
 	app.Use(gzip.Gzip(gzip.DefaultCompression)) //开启gzip压缩. 请求头中含 "Accept-Encoding : gzip" 才会压缩
 	g.GpsService.Init()
-	app.GET("/", g.routeGpsServer)
+	app.GET("/getcity", g.routeGpsServer)
 	endless.ListenAndServe(":"+strconv.Itoa(g.Port), app)
 }
 
@@ -50,28 +50,28 @@ func (g *GpsServer) routeGpsServer(ctx *gin.Context) {
 	lon_str := ctx.Request.FormValue("lon")
 	lat, _ := strconv.ParseFloat(lat_str, 64)
 	lon, _ := strconv.ParseFloat(lon_str, 64)
-	methodName := ctx.Request.FormValue("ac")
-
-	switch methodName {
-	case "GetGpsInfo":
-		ret = g.GpsService.FindByGps(lat, lon)
-		vi := reflect.ValueOf(ret)
-		if vi.IsNil() {
-			lib.Info("notfind lon:" + strconv.FormatFloat(lon, 'E', -1, 64) + " lat:" + strconv.FormatFloat(lat, 'E', -1, 64))
-		}
-		break
-	default:
-		errorCode = RET_ERR
-		errorMsg = "接口不存在"
+	ret = g.GpsService.FindByGps(lat, lon)
+	vi := reflect.ValueOf(ret)
+	if vi.IsNil() {
+		lib.Info("notfind lon:" + strconv.FormatFloat(lon, 'E', -1, 64) + " lat:" + strconv.FormatFloat(lat, 'E', -1, 64))
 	}
-
-	acElapsed := time.Since(beginTime) //接口耗时
-	echoResult(errorCode, errorMsg, ret, ctx)
+	jsonBytes, err := json.Marshal(ret)
+	if err!=nil{
+		panic(err)
+	}
+	var obj map[string]interface{}
+	err = json.Unmarshal(jsonBytes, &obj)
+	if err!=nil{
+		panic(err)
+	}
+	obj["lat"]=lat_str
+	obj["lon"]=lon_str
+	echoResult(errorCode, errorMsg, obj, ctx)
 	totalElapsed := time.Since(beginTime) //总耗时
 	if totalElapsed > 500*time.Millisecond {
 		//超过500毫秒
 		retJson, _ := json.Marshal(ret)
-		logMsg := "Elapsed Time: lat:" + lat_str + "|lon:" + lon_str + "|acElapsed:" + acElapsed.String() + "|totalElapsed:" + totalElapsed.String()
+		logMsg := "Elapsed Time: lat:" + lat_str + "|lon:" + lon_str + "|totalElapsed:" + totalElapsed.String()
 		logMsg += "|url:" + ctx.Request.RequestURI
 		logMsg += "|ret:" + string(retJson)
 		lib.SetInfoPath("/home/log/gpsServer/slow_access.log").Info("notfind lon:" + strconv.FormatFloat(lon, 'E', -1, 64) + " lat:" + strconv.FormatFloat(lat, 'E', -1, 64))
